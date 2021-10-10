7c34b365f3a0c756b4603a3db78149112da583ce
Chris Abadilla <chris@chriss-air.attlocal.net>
2021-10-10 13:21:38 -0700

    set up ready

---
+++ finalportfolio/src/Components/Projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Data } from "../Data.js";
+
+export const Projects = () => {
+    return (
+        <>
+            <div className="container">Here are the projects</div>
+            {Data.map((data, key) => {
+                return (
+                    <div key={key}>
+                        <h3>{data.title}</h3>
+                        <p>{data.description}</p>
+                    </div>
+                )
+            })}
+        </>
+    );
+};
